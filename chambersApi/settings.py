b03b5700e8b3d2d6c17acfdb243de73f813ac04c
Habeebdev1 <untamed737@gmail.com>
2023-01-15 11:43:43 +0100

    init

--- chambersApi/settings.py
+++ chambersApi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8qmudgt)=o5%oo(528bamp)(b8q914*89v8g%pje!&e71c^kiv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://web-production-e217.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -88,7 +88,6 @@ DATABASES = {
     }
 }
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-e217.up.railway.app']
 
 
 # Password validation
